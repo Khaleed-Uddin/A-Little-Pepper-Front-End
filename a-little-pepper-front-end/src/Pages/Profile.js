05250d5a3eb1e8f1bbde0e4feeb295bc82e7c360
Khaleed Uddin <uddin1786@gmail.com>
2022-12-04 15:51:49 -0500

    minor changes

--- a-little-pepper-front-end/src/Pages/Profile.js
+++ a-little-pepper-front-end/src/Pages/Profile.js
@@ -46,38 +46,38 @@ export default function Profile() {
 
   return (
     <div className="my-5" style={{ color: "black" }}>
-
-      <article className="mb-5">
-        <CalorieTracker />
-      </article>
-      <div>
-        <Card style={{ backgroundColor: "orange", alignItems: "center" }}>
-          <Card.Img
-            variant="top"
-            // src={profile.picture}
-            // style={{ width: "300px" ,borderRadius: "50%"}}
-          />
-          <Card.Body>
-            {/* <h1>lol</h1> */}
-            <Card.Title>{user.displayName}</Card.Title>
-            <Card.Text>Calories: {profile.cal}</Card.Text>
-            <Card.Text>Fat: {profile.fat}</Card.Text>
-            <Card.Text>Carbs: {profile.carb}</Card.Text>
-            <Card.Text>Protein: {profile.protein}</Card.Text>
-          </Card.Body>
-        </Card>
-        <div
-          className="d-flex align-items-center justify-content-center"
-          style={{ gap: ".5rem" }}
-        >
-          <Button variant="light" onClick={handleSignOut}>
-            Sign Out
-          </Button>
-        </div>
-        {/* <section className="my-5">
-          <h2>Bookmarked Recipes</h2>
-          <Row xs={1} md={2} lg={3} className="g-5 py-5">
-
+      {profile.id ? (
+        <>
+          <article className="mb-5">
+            <CalorieTracker />
+          </article>
+          <div>
+            <Card style={{ backgroundColor: "orange", alignItems: "center" }}>
+              <Card.Img
+                variant="top"
+                // src={profile.picture}
+                // style={{ width: "300px" ,borderRadius: "50%"}}
+              />
+              <Card.Body>
+                {/* <h1>lol</h1> */}
+                <Card.Title>{user.displayName}</Card.Title>
+                <Card.Text>Calories: {profile.cal}</Card.Text>
+                <Card.Text>Fat: {profile.fat}</Card.Text>
+                <Card.Text>Carbs: {profile.carb}</Card.Text>
+                <Card.Text>Protein: {profile.protein}</Card.Text>
+              </Card.Body>
+            </Card>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ gap: ".5rem" }}
+            >
+              <Button variant="light" onClick={handleSignOut}>
+                Sign Out
+              </Button>
+            </div>
+            <section className="my-5">
+              <h2>Bookmarked Recipes</h2>
+              {/* <Row xs={1} md={2} lg={3} className="g-5 py-5">
             {savedRecipes.map((recipe) => {
               return (
                 <>
@@ -85,7 +85,6 @@ export default function Profile() {
                 </>
               );
             })}
-
           </Row> */}
             </section>
           </div>
@@ -95,11 +94,18 @@ export default function Profile() {
           Create Profile
         </Button>
       )}
-
-          </Row>
-        </section> */}
-      </div>
-
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
